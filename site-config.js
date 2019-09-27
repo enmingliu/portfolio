63950d93a693309ebb95c1e440db53b54a311c3f
Bill Liu <bill.liu.school@gmail.com>
2019-09-26 22:17:51 -0700

    updated package and site-config

--- site-config.js
+++ site-config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
+  siteTitle: `Bill Liu`,
+  siteTitleShort: `BLiu`,
+  siteDescription: `Bill Liu's personal website`,
+  siteUrl: `https://bliu.me`,
   themeColor: `#000`,
   backgroundColor: `#fff`,
   pathPrefix: null,
